792be0e184637be0641683a31bbe56b2da88c6d8
Samuele Giani <samuele.giani@gmail.com>
2025-05-31 19:38:27 +0200

    refacto1

---
+++ PDFs/quartize.sh
@@ -0,0 +1,10 @@
+
+cp ../*ipynb .
+
+quarto render ./Acetone_synthesis.ipynb --to pdf
+quarto render ./BHC_Company_synthesis_of_ibuprofen.ipynp --to pdf
+quarto render ./Boots_Company_synthesis_of_ibuprofen.ipynb --to pdf
+quarto render ./Markdown_CheatSheet.ipynb --to pdf
+quarto render ./README.ipynb --to pdf
+quarto render ./Untitled1.ipynb --to pdf
+quarto render ./Untitled.ipynb --to pdf
